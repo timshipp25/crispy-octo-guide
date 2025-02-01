1c36299eb1b415e2d8397c9f2362661551d55f2a
timshipp25 <tim_shipp@hotmail.com>
2025-02-01 23:39:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,37 @@
 from datetime import datetime
 from discord_webhook import DiscordWebhook
 import os
+import asyncio
+from sql_module_gh import fetch_ip_addresses
+
+async def allCode(fail_streak):
+    ip_addresses = await fetch_ip_addresses(fail_streak)
+    for ip in ip_addresses:
+        print("IP Address:", ip[0])
+    return ip_addresses
+
+async def send_webhook(formatted_time):
+
+    ip = await allCode(1)
+    outtxt = formatted_time + " : " + ip[0]
+
+    try:
+        SOME_SECRET = os.environ["SOME_SECRET"]
+        thisurl = f"https://discordapp.com/api/webhooks/{SOME_SECRET}"
+        webhook = DiscordWebhook(url=thisurl, content=outtxt)
+        response = await webhook.execute()
+    except KeyError:
+        SOME_SECRET = "Token not available!"
+
+    return
 
 # Get current date and time
 current_time = datetime.now()
 formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
-try:
-    SOME_SECRET = os.environ["SOME_SECRET"]
-    thisurl = f"https://discordapp.com/api/webhooks/{SOME_SECRET}"
-    webhook = DiscordWebhook(url=thisurl, content=formatted_time)
-    response = webhook.execute()
-except KeyError:
-    SOME_SECRET = "Token not available!"
+# Run the async function
+asyncio.run(send_webhook(formatted_time))
 
 # Write to file
 with open('current_datetime.txt', 'w') as f:
-    f.write(f'Current Date and Time: {formatted_time} : {SOME_SECRET}')
+    f.write(f'Current Date and Time: {formatted_time}')
