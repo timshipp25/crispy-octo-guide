eda7571656da3d93231da898ab848279ed764a52
timshipp25 <tim_shipp@hotmail.com>
2025-02-01 12:29:59 +0000

    Update main.py

--- main.py
+++ main.py
@@ -4,14 +4,15 @@ import os
 
 # Get current date and time
 current_time = datetime.now()
+formatted_time = current_time.strftime("%Y-%m-%d %H:%M:%S")
 
 try:
     SOME_SECRET = os.environ["SOME_SECRET"]
-    webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/{SOME_SECRET}", content=current_time)
+    webhook = DiscordWebhook(url="https://discordapp.com/api/webhooks/{SOME_SECRET}", content=formatted_time)
     response = webhook.execute()
 except KeyError:
     SOME_SECRET = "Token not available!"
 
 # Write to file
 with open('current_datetime.txt', 'w') as f:
-    f.write(f'Current Date and Time: {current_time} : {SOME_SECRET}')
+    f.write(f'Current Date and Time: {formatted_time} : {SOME_SECRET}')
